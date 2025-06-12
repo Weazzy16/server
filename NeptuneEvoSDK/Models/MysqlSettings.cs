10c84e56e1cf72ad79e27d226e00a4b751e18302
Weazzy16 <91275348+Weazzy16@users.noreply.github.com>
2025-06-12 18:12:10 +0300

    Delete NeptuneEvoSDK directory

--- NeptuneEvoSDK/Models/MysqlSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Redage.SDK.Models
-{
-    public class MysqlSettings : Mysql
-    {
-        public List<Mysql> OtherList = new List<Mysql>();
-    }
-}
