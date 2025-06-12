f7e2e3866a5d2dee04936c07fcb932fb9acfdc69
Weazzy16 <Weazzy4x@gmail.com>
2025-06-12 18:44:41 +0300

    Remove all files for fresh upload

--- src_cef/src/views/eternal-dev/containers/modules/prize.js
+++
@@ -1,3 +0,0 @@
-export const getPrizePicture = (model) => {
-    return `https://cdn.majestic-files.com/public/master/static/img/vehicles/${model.toLowerCase()}.png`;
-}
--- src_cef/src/views/player/hudevo/stores.js
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import {executeClientToGroup} from "api/rage";
-
-export const currentWeather = writable("thunder");
